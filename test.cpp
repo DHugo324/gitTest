074478a19706d51aa0d40b1a5daff4ff652ba1ac
DHugo324 <hugodai0324@gmail.com>
2023-11-05 21:57:09 +0800

    init commit

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FASTER ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long ll;
+#define MOD 1e9 + 7
+#define mem(arr, x) memset(arr, x, sizeof(arr))
+#define PB push_back
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+/*===== Just_a_Line =====*/
+void solve() {
+  cout << "Test\n";
+}
+int main() {
+  FASTER;
+  int t = 1;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
