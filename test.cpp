5841f998648092781251baa4ad99d53a5486ac82
DHugo324 <hugodai0324@gmail.com>
2023-11-05 22:01:40 +0800

    Update on branch

--- test.cpp
+++ test.cpp
@@ -9,7 +9,7 @@ typedef long long ll;
 #define pll pair<long long,long long>
 /*===== Just_a_Line =====*/
 void solve() {
-  cout << "Test\n";
+  cout << "Let's test on branch\n";
 }
 int main() {
   FASTER;
